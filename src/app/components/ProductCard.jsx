ab1aaa51be97e8d020072498855337e418e7cc8b
Appleeyes <appleeyespackage@gmail.com>
2024-07-26 14:40:19 +0100

    feat: implemented bookmark feature

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import '../assets/css/ProductCard.css';
-import Rating from '../assets/images/rating.svg';
-import Location from '../assets/images/location.svg';
-import Love from '../assets/images/love-icon.svg';
-import Footer from '../components/Footer';
+import React, { useState } from "react";
+import axios from "axios";
+import "../assets/css/ProductCard.css";
+import Rating from "../assets/images/rating.svg";
+import Location from "../assets/images/location.svg";
+import Love from "../assets/images/love-icon.svg";
+import Loved from "../assets/images/loved.svg";
+import Footer from "../components/Footer";
 
 function ProductCard({
   ProductImage,
@@ -12,7 +14,39 @@ function ProductCard({
   ProductTitle,
   ProductLocation,
   ProductId,
+  isBookmarked: initialIsBookmarked = false,
 }) {
+  const [isBookmarked, setIsBookmarked] = useState(initialIsBookmarked);
+  const [error, setError] = useState(null);
+
+  const toggleBookmark = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      console.error("Authentication token not found");
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        `https://myhome-ng-backend.onrender.com/api/v1/products/${ProductId}/bookmark`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setIsBookmarked(!isBookmarked);
+      console.log(response.data.message);
+    } catch (err) {
+      if (err.response) {
+        setError(err.response.data.message);
+      } else {
+        setError("An unexpected error occurred");
+      }
+    }
+  };
+
   return (
     <div className="product">
       <div className="product-image">
@@ -34,8 +68,14 @@ function ProductCard({
           <a href={`/product/overview/${ProductId}`}>Explore</a>
         </div>
         <div className="love-img">
-          <img src={Love} alt="Love Icon" />
+          <img
+            src={isBookmarked ? Loved : Love}
+            alt="Love Icon"
+            onClick={toggleBookmark}
+            className={isBookmarked ? "bookmarked" : ""}
+          />
         </div>
+        {error && <div className="error">{error}</div>}
       </div>
       <Footer currentRoute={window.location.pathname} />
     </div>
--- src/app/pages/SignupRole.jsx
+++ src/app/pages/SignupRole.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { useHistory } from "react-router-dom";
 import "../assets/css/OnboardingSteps.css";
 import Button from "../components/ButtonComponent";
